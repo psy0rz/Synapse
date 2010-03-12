e66e71f3618fd2985523c9428b8874d6ffe1d04b
edwin <edwin@99606f1f-bbb2-4562-8e6b-387ddef1dc61>
2010-03-12 15:28:25 +0000

    fixed console loggging
    git-svn-id: https://open.syn3.nl/syn3/svndav/default/trunk/projects/synapse@251 99606f1f-bbb2-4562-8e6b-387ddef1dc61

--- wwwdir/synapse.js
+++ wwwdir/synapse.js
@@ -1,4 +1,32 @@
+//disable logging if we dont have firebug.
+//we replace this with custom logging later perhaps.
+if (typeof console == 'undefined')
+{
+	function Cconsole(){
+	};
+	Cconsole.prototype.debug=function() { };
+	Cconsole.prototype.info=function() { };
+	Cconsole.prototype.log=function() { };
+	Cconsole.prototype.error=function() { };
+
+	console=new Cconsole();
+}
+
+console.debug("dsa");
+
 var synapse_handlers=new Array();
+
+
+// function send(msg_dst, msg_event, msg)
+// {
+// /*	$.ajax({
+// 
+// 		dataType: "json",
+// 		url: '/synapse/send',
+// 		success: synapse_handleMessages,
+// 		error: synapse_handleError
+// 	});*/
+// }
 
 function synapse_register(event, handler)
 {
@@ -7,7 +35,10 @@ function synapse_register(event, handler)
 
 function synapse_handleError(request, status)
 {
-
+	if (synapse_handlers["error"])
+	{
+		synapse_handlers["error"]("Error while processing ajax request: " + status);
+	}
 }
 
 function synapse_handleMessages(messages, status)
@@ -25,6 +56,8 @@ function synapse_handleMessages(messages, status)
 	for (var messageI in messages)
 	{
 		message=messages[messageI];
+		console.debug("handling:",message);
+	
 		try
 		{
 			//handler exists?
@@ -62,8 +95,13 @@ function synapse_handleMessages(messages, status)
 	});
 }
 
+
+
 $(document).ready(function(){
 	//place code inside this block:
+
 	synapse_handleMessages(Array());
 });
+
+
 
