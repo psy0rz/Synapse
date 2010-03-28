5d6208376bcb11cef66460624ef3e51539124a75
edwin <edwin@99606f1f-bbb2-4562-8e6b-387ddef1dc61>
2010-03-28 15:01:10 +0000

    now its possible to register multiple handlers per event
    git-svn-id: https://open.syn3.nl/syn3/svndav/default/trunk/projects/synapse@286 99606f1f-bbb2-4562-8e6b-387ddef1dc61

--- wwwdir/synapse.js
+++ wwwdir/synapse.js
@@ -37,15 +37,45 @@ function synapse_receive()
 	});
 }
 
+function synapse_register(event, handler)
+{
+	if (typeof synapse_handlers[event] == 'undefined')
+	{
+		synapse_handlers[event]=new Array();
+	}
+	synapse_handlers[event].push(handler);
+}
+
+function synapse_callHandlers(event)
+{
+	if (synapse_handlers[event])
+	{
+		for (var handlerI in synapse_handlers[event])
+		{
+			try	
+			{
+				synapse_handlers[event][handlerI](arguments[1], arguments[2], arguments[3], arguments[4])
+			}
+			catch(e)
+			{
+				console.error("Error while calling handler", e);
+				if (event!="error")
+				{
+					synapse_callHandlers("error", "Exception error: "+e.message);
+				}
+			}
+
+		}
+		return true;		
+	}	
+	return false;
+}
 
 function synapse_handleError(request, status, e)
 {
 	errorTxt="Error while processing request: " + request.responseText;
 	console.error(errorTxt);
-	if (synapse_handlers["error"])
-	{
-		synapse_handlers["error"](errorTxt);
-	}
+	synapse_callHandlers("error", errorTxt);
 }
 
 //handles long poll results, which contain incoming messages
@@ -59,11 +89,8 @@ function synapse_handleMessages(messages, status, XMLHttpRequest)
 
 	if (messages==null)
 	{
-		if (synapse_handlers["error"])
-		{
-			synapse_handlers["error"]("Got null json reply, stopping.");
-			synapse_stop=true;
-		}
+		synapse_callHandlers("error", "Connection error with server");
+		synapse_stop=true;
 		return;
 	}
 	
@@ -73,31 +100,10 @@ function synapse_handleMessages(messages, status, XMLHttpRequest)
 		message=messages[messageI];
 		console.debug("handling:",message);
 	
-		try
+		if (!synapse_callHandlers(message[2], message[0], message[1], message[2], message[3]))
 		{
-			//handler exists?
-			if (synapse_handlers[message[2]])
-			{
-				//call it
-				synapse_handlers[message[2]](message[0], message[1], message[2], message[3]);
-			}
-			//default handler exists?
-			else
-			{
-				if (synapse_handlers["default"])
-				{
-					//call it
-					synapse_handlers["default"](message[0], message[1], message[2], message[3]);
-				}
-			}
-		}
-		catch(e)
-		{
-			if (synapse_handlers["error"])
-			{
-				//call it
-				synapse_handlers["error"](e.description);
-			}
+			//not found, try defaul handler
+			synapse_callHandlers("default", message[0], message[1], message[2], message[3]);
 		}
 	}
 
@@ -108,10 +114,6 @@ function synapse_handleMessages(messages, status, XMLHttpRequest)
 }
 
 
-function synapse_register(event, handler)
-{
-	synapse_handlers[event]=handler;
-}
 
 function send(msg_dst, msg_event, msg)
 {
