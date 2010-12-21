1be6cdd3c3d58100d86b66d38ec02b31f29ff289
Edwin Eefting <edwin@datux.nl>
2010-12-21 23:18:44 +0100

    cleaned up wwwdir structure

--- wwwdir/synapse.js
+++
@@ -1,8 +0,0 @@
-document.write('<link href="css/redmond/jquery-ui.css" rel="stylesheet" type="text/css"/>');
-document.write('<script type="text/javascript" src="jquery.js"></scr'+'ipt>');
-document.write('<script type="text/javascript" src="jquery-ui.js"></scr'+'ipt>');
-document.write('<script type="text/javascript" src="json2.js"></scr'+'ipt>');
-document.write('<script type="text/javascript" src="jquery.cooquery.js"></scr'+'ipt>');
-document.write('<script type="text/javascript" src="synapse-core.js"></scr'+'ipt>');
-document.write('<script type="text/javascript" src="synapse-ui.js"></scr'+'ipt>');
-
---
+++ wwwdir/synapse/synapse.js
@@ -0,0 +1,8 @@
+document.write('<link href="/synapse/css/redmond/jquery-ui.css" rel="stylesheet" type="text/css"/>');
+document.write('<script type="text/javascript" src="/synapse/jquery.js"></scr'+'ipt>');
+document.write('<script type="text/javascript" src="/synapse/jquery-ui.js"></scr'+'ipt>');
+document.write('<script type="text/javascript" src="/synapse/json2.js"></scr'+'ipt>');
+document.write('<script type="text/javascript" src="/synapse/jquery.cooquery.js"></scr'+'ipt>');
+document.write('<script type="text/javascript" src="/synapse/synapse-core.js"></scr'+'ipt>');
+document.write('<script type="text/javascript" src="/synapse/synapse-ui.js"></scr'+'ipt>');
+
