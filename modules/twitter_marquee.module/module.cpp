c386a019a338da2166fbbffead09b862ba893e63
Edwin Eefting <edwin@datux.nl>
2011-05-06 17:34:16 +0200

    vetter gemaakt

--- modules/twitter_marquee.module/module.cpp
+++ modules/twitter_marquee.module/module.cpp
@@ -21,23 +21,24 @@
 
 using namespace std;
 
-string statusStr;
-string errorStr;
-bool ok;
+string gStatusStr;
+string gErrorStr;
+bool gOk;
 
-synapse::Cconfig config;
+synapse::Cconfig gConfig;
 
-Cvar statusses;
+//active tweets that are shown on the bar
+Cvar gTweets;
 
 void setMarquee()
 {
 	Cmsg out;
 	out.event="marquee_Set";
 	out["classes"].list().push_back(string("twitter"));
-	if (!ok)
-		out["text"]=errorStr;
+	if (!gOk)
+		out["text"]=gErrorStr;
 	else
-		out["text"]=statusStr;
+		out["text"]=gStatusStr;
 
 	out.send();
 
@@ -45,8 +46,8 @@ void setMarquee()
 
 SYNAPSE_REGISTER(module_Init)
 {
-	ok=false;
-	config.load("etc/synapse/twitter_marquee.conf");
+	gOk=false;
+	gConfig.load("etc/synapse/twitter_marquee.conf");
 	
 	Cmsg out;
 	out.event="core_LoadModule";
@@ -74,45 +75,158 @@ SYNAPSE_REGISTER(twitter_Ready)
 
 SYNAPSE_REGISTER(twitter_Ok)
 {
-	ok=true;
-	errorStr="";
+	gOk=true;
+	gErrorStr="";
 	setMarquee();
 }
 
 SYNAPSE_REGISTER(twitter_Error)
 {
-	ok=false;
-	errorStr=msg["error"].str();
+	gOk=false;
+	gErrorStr=msg["error"].str();
 	setMarquee();
 }
 
+/*
+ (map:)
+ |delete = (map:)
+ | |status = (map:)
+ | | |id = 1.00807e+08 (long double)
+ | | |id_str = 66497058894393344 (string)
+
+
+0x2670870 SEND twitter_Data FROM 5:module@twitter TO broadcast (2:module@twitter_marquee 7:module@http_json ) (map:)
+ |contributors = (empty)
+ |coordinates = (empty)
+ |created_at = Fri May 06 14:26:34 +0000 2011 (string)
+ |entities = (map:)
+ | |hashtags = (empty list)
+ | |urls = (empty list)
+ | |user_mentions = (list:)
+ | | |(map:)
+ | | | |id = 2.45697e+08 (long double)
+ | | | |id_str = 245697242 (string)
+ | | | |indices = (list:)
+ | | | | |0 (long double)
+ | | | | |15 (long double)
+ | | | |name = Heleen (string)
+ | | | |screen_name = heleenkerssies (string)
+ |favorited = 0 (long double)
+ |geo = (empty)
+ |id = 6.04119e+08 (long double)
+ |id_str = 66509162615545856 (string)
+ |in_reply_to_screen_name = heleenkerssies (string)
+ |in_reply_to_status_id = 1.99243e+09 (long double)
+ |in_reply_to_status_id_str = 66477784972791808 (string)
+ |in_reply_to_user_id = 2.45697e+08 (long double)
+ |in_reply_to_user_id_str = 245697242 (string)
+ |place = (empty)
+ |retweet_count = 0 (long double)
+ |retweeted = 0 (long double)
+ |source = web (string)
+ |text = @heleenkerssies hij doet het al :) (string)
+ |truncated = 0 (long double)
+ |user = (map:)
+ | |contributors_enabled = 0 (long double)
+ | |created_at = Tue Nov 16 23:18:31 +0000 2010 (string)
+ | |default_profile = 1 (long double)
+ | |default_profile_image = 1 (long double)
+ | |description = (empty)
+ | |favourites_count = 1 (long double)
+ | |follow_request_sent = (empty)
+ | |followers_count = 13 (long double)
+ | |following = (empty)
+ | |friends_count = 16 (long double)
+ | |geo_enabled = 0 (long double)
+ | |id = 2.1652e+08 (long double)
+ | |id_str = 216519907 (string)
+ | |is_translator = 0 (long double)
+ | |lang = en (string)
+ | |listed_count = 0 (long double)
+ | |location = (empty)
+ | |name = Edwin Eefting (string)
+ | |notifications = (empty)
+ | |profile_background_color = C0DEED (string)
+ | |profile_background_image_url = http://a3.twimg.com/a/1303425044/images/themes/theme1/bg.png (string)
+ | |profile_background_tile = 0 (long double)
+ | |profile_image_url = http://a3.twimg.com/sticky/default_profile_images/default_profile_2_normal.png (string)
+ | |profile_link_color = 0084B4 (string)
+ | |profile_sidebar_border_color = C0DEED (string)
+ | |profile_sidebar_fill_color = DDEEF6 (string)
+ | |profile_text_color = 333333 (string)
+ | |profile_use_background_image = 1 (long double)
+ | |protected = 0 (long double)
+ | |screen_name = EHEefting (string)
+ | |show_all_inline_media = 0 (long double)
+ | |statuses_count = 42 (long double)
+ | |time_zone = (empty)
+ | |url = (empty)
+ | |utc_offset = (empty)
+ | |verified = 0 (long double)
+
+
+ */
 SYNAPSE_REGISTER(twitter_Data)
 {
+	bool changed=false;
+
+	//new twitter message
 	if (msg.isSet("user"))
 	{
 		string name=msg["user"]["screen_name"];
-		statusses[name].list().push_front(msg["text"].str());
 
-		while ((long)statusses[name].list().size() > config["show"][name])
+		//not too old and a special user or has the twitbar hashtag
+		if (	gConfig["show"].isSet(name) ||
+				msg["text"].str().find(gConfig["keyword"].str())!=string::npos)
 		{
-			statusses[name].list().pop_back();
-		}
+			//add it
+			gTweets.list().push_front(msg);
 
-		//now format a nice status string
-		statusStr="%C2TWITTER ";
-		for (	Cvar::iterator nameI=statusses.begin();
-				nameI!=statusses.end();
-				nameI++)
-		{
-			for (	CvarList::iterator statusI=nameI->second.list().begin();
-					statusI!=nameI->second.list().end();
-					statusI++)
+			//how many messages this user may do?
+			int maxTweets=1;
+			if (gConfig["show"].isSet(name))
+				maxTweets=gConfig["show"][name];
+
+			//count the messages for this user
+			CvarList::iterator lastTweetI;
+			int tweetCount=0;
+			for (	CvarList::iterator tweetI=gTweets.list().begin();
+					tweetI!=gTweets.list().end();
+					tweetI++)
 			{
-				statusStr+="%C6"+nameI->first+":%C0"+statusI->str()+"  ";
+				if ((*tweetI)["user"]["screen_name"].str()==name)
+				{
+					lastTweetI=tweetI;
+					tweetCount++;
+				}
 			}
+
+			//too much? delete last one
+			if (tweetCount>maxTweets)
+				gTweets.list().erase(lastTweetI);
+
+			changed=true;
 		}
+	}
+
+	if (changed)
+	{
+		//now format a nice status string
+		gStatusStr="%S1Webba TWITBAR  ";
+		for (	CvarList::iterator tweetI=gTweets.list().begin();
+				tweetI!=gTweets.list().end();
+				tweetI++)
+		{
+
+			gStatusStr+="%C7"+(*tweetI)["user"]["screen_name"].str()+":%C5"+(*tweetI)["text"].str()+"  ";
+		}
+
+		//filter stuff out we dont want
+
+
 		setMarquee();
 	}
+
 }
 
 
