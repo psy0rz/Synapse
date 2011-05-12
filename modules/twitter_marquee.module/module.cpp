17467803bf18ff55f8be237c59ffd2dd0b745bed
Edwin Eefting <edwin@datux.nl>
2011-05-12 14:36:02 +0200

    handles retweets nicely

--- modules/twitter_marquee.module/module.cpp
+++ modules/twitter_marquee.module/module.cpp
@@ -166,6 +166,121 @@ SYNAPSE_REGISTER(twitter_Error)
  | |verified = 0 (long double)
 
 
+
+0x16b9de0 RECV twitter_Data FROM 5 BY 7:module@http_json (map:)
+ |contributors = (empty)
+ |coordinates = (empty)
+ |created_at = Wed May 11 19:40:10 +0000 2011 (string)
+ |favorited = 0 (long double)
+ |geo = (empty)
+ |id = -9.81324e+08 (long double)
+ |id_str = 68400020382167040 (string)
+ |in_reply_to_screen_name = (empty)
+ |in_reply_to_status_id = (empty)
+ |in_reply_to_status_id_str = (empty)
+ |in_reply_to_user_id = (empty)
+ |in_reply_to_user_id_str = (empty)
+ |place = (empty)
+ |retweet_count = 0 (long double)
+ |retweeted = 0 (long double)
+ |retweeted_status = (map:)
+ | |contributors = (empty)
+ | |coordinates = (empty)
+ | |created_at = Wed May 11 17:33:38 +0000 2011 (string)
+ | |favorited = 0 (long double)
+ | |geo = (empty)
+ | |id = 1.33393e+09 (long double)
+ | |id_str = 68368179809890306 (string)
+ | |in_reply_to_screen_name = (empty)
+ | |in_reply_to_status_id = (empty)
+ | |in_reply_to_status_id_str = (empty)
+ | |in_reply_to_user_id = (empty)
+ | |in_reply_to_user_id_str = (empty)
+ | |place = (empty)
+ | |retweet_count = 0 (long double)
+ | |retweeted = 0 (long double)
+ | |source = <a href="http://www.tweetdeck.com" rel="nofollow">TweetDeck</a> (string)
+ | |text = De heren @stefankuhl en @edwingels namens Webba (www.webba.nl) opdracht gegeven om voor @RooBeekAdvies aan de slag te gaan. Succes heren! (string)
+ | |truncated = 0 (long double)
+ | |user = (map:)
+ | | |contributors_enabled = 0 (long double)
+ | | |created_at = Tue May 03 05:35:33 +0000 2011 (string)
+ | | |default_profile = 1 (long double)
+ | | |default_profile_image = 0 (long double)
+ | | |description = Advies en interim / Ruimte en omgeving (ruimtelijke ordening; planologie) / Vergunning en Wabo (bouwen; vergunningmanagement) / Training en coachen   (string)
+ | | |favourites_count = 0 (long double)
+ | | |follow_request_sent = 0 (long double)
+ | | |followers_count = 9 (long double)
+ | | |following = 0 (long double)
+ | | |friends_count = 0 (long double)
+ | | |geo_enabled = 0 (long double)
+ | | |id = 2.9215e+08 (long double)
+ | | |id_str = 292149567 (string)
+ | | |is_translator = 0 (long double)
+ | | |lang = en (string)
+ | | |listed_count = 0 (long double)
+ | | |location = Zuidoost Drenthe (string)
+ | | |name = RooBeek Advies (string)
+ | | |notifications = 0 (long double)
+ | | |profile_background_color = C0DEED (string)
+ | | |profile_background_image_url = http://a3.twimg.com/images/themes/theme1/bg.png (string)
+ | | |profile_background_tile = 0 (long double)
+ | | |profile_image_url = http://a2.twimg.com/profile_images/1336791351/9928661_normal.jpg (string)
+ | | |profile_link_color = 0084B4 (string)
+ | | |profile_sidebar_border_color = C0DEED (string)
+ | | |profile_sidebar_fill_color = DDEEF6 (string)
+ | | |profile_text_color = 333333 (string)
+ | | |profile_use_background_image = 1 (long double)
+ | | |protected = 0 (long double)
+ | | |screen_name = RooBeekAdvies (string)
+ | | |show_all_inline_media = 0 (long double)
+ | | |statuses_count = 1 (long double)
+ | | |time_zone = (empty)
+ | | |url = http://www.roobeek-advies.nl (string)
+ | | |utc_offset = (empty)
+ | | |verified = 0 (long double)
+ |source = <a href="http://www.tweetdeck.com" rel="nofollow">TweetDeck</a> (string)
+ |text = RT @RooBeekAdvies: De heren @stefankuhl en @edwingels namens Webba (www.webba.nl) opdracht gegeven om voor @RooBeekAdvies aan de slag te ... (string)
+ |truncated = 1 (long double)
+ |user = (map:)
+ | |contributors_enabled = 0 (long double)
+ | |created_at = Thu Aug 20 17:05:07 +0000 2009 (string)
+ | |default_profile = 0 (long double)
+ | |default_profile_image = 0 (long double)
+ | |description = Webba, full service Internet & Reclamebureau (string)
+ | |favourites_count = 0 (long double)
+ | |follow_request_sent = 0 (long double)
+ | |followers_count = 282 (long double)
+ | |following = 0 (long double)
+ | |friends_count = 160 (long double)
+ | |geo_enabled = 0 (long double)
+ | |id = 6.73626e+07 (long double)
+ | |id_str = 67362552 (string)
+ | |is_translator = 0 (long double)
+ | |lang = en (string)
+ | |listed_count = 12 (long double)
+ | |location = Emmen (string)
+ | |name = Webba_nl (string)
+ | |notifications = 0 (long double)
+ | |profile_background_color = 039ce3 (string)
+ | |profile_background_image_url = http://a0.twimg.com/profile_background_images/169845957/ontwerp-achtergrond.jpg (string)
+ | |profile_background_tile = 0 (long double)
+ | |profile_image_url = http://a0.twimg.com/profile_images/1072739333/webba_normal.jpg (string)
+ | |profile_link_color = 009de0 (string)
+ | |profile_sidebar_border_color = 4b4a4d (string)
+ | |profile_sidebar_fill_color = f2eded (string)
+ | |profile_text_color = 575757 (string)
+ | |profile_use_background_image = 1 (long double)
+ | |protected = 0 (long double)
+ | |screen_name = webba_nl (string)
+ | |show_all_inline_media = 0 (long double)
+ | |statuses_count = 778 (long double)
+ | |time_zone = Amsterdam (string)
+ | |url = http://www.webba.nl (string)
+ | |utc_offset = 3600 (long double)
+ | |verified = 0 (long double)
+
+
  */
 SYNAPSE_REGISTER(twitter_Data)
 {
@@ -247,8 +362,18 @@ SYNAPSE_REGISTER(twitter_Data)
 		gStatusStr=gConfig["main_header"].str();
 		FOREACH_VARLIST(tweet, gTweets)
 		{
-
-			string text=tweet["text"];
+			string text;
+			string name;
+			if (tweet.isSet("retweeted_status"))
+			{
+				text=tweet["retweeted_status"]["text"].str();
+				name=tweet["retweeted_status"]["user"]["screen_name"].str();
+			}
+			else
+			{
+				text=tweet["text"].str();
+				name=tweet["user"]["screen_name"].str();
+			}
 
 			//filter stuff out we dont want
 			FOREACH_VARMAP(replace, gConfig["replace"])
@@ -263,7 +388,7 @@ SYNAPSE_REGISTER(twitter_Data)
 
 
 			gStatusStr+=gConfig["name_header"].str();
-			gStatusStr+=tweet["user"]["screen_name"].str();
+			gStatusStr+=name;
 			gStatusStr+=gConfig["text_header"].str();
 			gStatusStr+=text;
 		}
