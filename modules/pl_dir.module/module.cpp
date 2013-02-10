4756b5b25a19357b5e50e385df3e796348965160
Edwin Eefting <edwin@datux.nl>
2013-02-10 23:19:00 +0100

    update mode buttons

--- modules/pl_dir.module/module.cpp
+++ modules/pl_dir.module/module.cpp
@@ -127,7 +127,7 @@ namespace pl
 
 		static bool compareDate (Cdirectory_entry first, Cdirectory_entry second)
 		{
-			return (first.getDate() < second.getDate());
+			return (first.getDate() > second.getDate());
 		}
 
 
