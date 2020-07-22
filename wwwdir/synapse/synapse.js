23ed730eeb45a50f5be65b0bf1f28f238fedadb6
Edwin Eefting <edwin@datux.nl>
2020-07-22 12:45:26 +0200

    fix

--- wwwdir/synapse/synapse.js
+++ wwwdir/synapse/synapse.js
@@ -1,4 +1,4 @@
-//document.write('<link href="/css/redmond/jquery-ui.css" rel="stylesheet" type="text/css"/>');
+document.write('<link href="/css/redmond/jquery-ui.css" rel="stylesheet" type="text/css"/>');
 document.write('<script type="text/javascript" src="/lib/jquery-1.9.0.js"></scr'+'ipt>');
 document.write('<script type="text/javascript" src="/lib/jquery-ui-1.10.0.custom.min.js"></scr'+'ipt>');
 document.write('<script type="text/javascript" src="/lib/json2.js"></scr'+'ipt>');
